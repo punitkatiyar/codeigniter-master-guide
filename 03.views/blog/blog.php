31adcb4c89d9d0e24b476c22fd04e9f8c69f440d
Punit Katiyar <punit.kat@gmail.com>
2022-01-05 18:22:24 +0530

    Add files via upload

---
+++ 03.views/blog/blog.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="src/css/style.css">
+</head>
+
+<body>
+    <header>CI Blog</header>
+    <nav>
+        <ul>
+            <li><a href=""><i class="fas fa-home"></i> Home</a></li>
+            <li><a href="">HTML Module</a></li>
+            <li><a href="">CSS Module</a></li>
+            <li><a href="">Bootstrap Module</a></li>
+            <li><a href="">PHP Module</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </nav>
+    <div class="banner">
+        <img src="src/image/course-1.jpg" width="100%" height="600px">
+    </div>
+    <h1>Welcome to tab 1</h1>
+    <h1>Welcome to tab 2</h1>
+    <h1>Welcome to tab 3</h1>
+    <h1>Welcome to tab 4</h1>
+    <h1>Welcome to tab 5</h1>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="src/js/app.js"></script>
+    <script src="https://kit.fontawesome.com/7fac20cd86.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>

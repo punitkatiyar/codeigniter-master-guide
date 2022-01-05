1dc1af551dcd660a7e9130c4cc4419036cd701c8
Punit Katiyar <punit.kat@gmail.com>
2022-01-05 18:14:17 +0530

    Add files via upload

---
+++ 02.controler/Welcome.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		// echo "Hello In Controler";
+		// echo 100+200;
+		//$this->load->view('test');
+		$this->load->view("blog/blog");
+	}
+	public function bootstrap()
+	{
+		// echo "Hello In Controler";
+		// echo 100+200;
+		//$this->load->view('test');
+		$this->load->view("blog/bootstrap");
+	}
+	
+}

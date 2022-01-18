eb7bb6bee0cdb1192350ab1256124f40275e6e1f
Punit Katiyar <punit.kat@gmail.com>
2022-01-18 18:04:45 +0530

    Add files via upload

---
+++ crud-app/template/mid.php
@@ -0,0 +1,14 @@
+<div class="container my-2 border">
+    <div class="row">
+        <div class="col-lg-9">
+            <div class="display-3 txt-primary text-center">Welcome to CI CRUD App</div>
+
+        </div>
+        <div class="col-lg-3">
+            <?= base_url();?>
+
+
+        </div>
+    </div>
+
+</div>

0ab2bdf11f19181213ef40e0a741753c2351bbe7
Punit Katiyar <punit.kat@gmail.com>
2022-01-19 18:13:10 +0530

    Update controler.php

--- crud-app/controler.php
+++ crud-app/controler.php
@@ -2,6 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Crud extends CI_Controller {
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Cimodel');
+
+    }
+
+
 	public function index()
 	{
         $this->load->view("template/head");
@@ -10,14 +18,26 @@ class Crud extends CI_Controller {
 	}
     public function dinsert()
 	{
+		if(isset($_POST['insert']))
+		{
+			//$uid=$_POST['uid'];
+			//extract($_POST);
+			$data['uname']=$this->input->post('uname');
+			$data['uemail']=$this->input->post('uemail');
+			$data['upass']=$this->input->post('upass');
+			$data['ucity']=$this->input->post('ucity');
+			$data['ucourse']=$this->input->post('ucourse');
+			$this->Cimodel->ciinsert($data);
+		}
 		$this->load->view("template/head");
         $this->load->view('template/insert');
 		$this->load->view('template/footer');
 	}
     public function dview()
 	{
+		$data['arr']=$this->Cimodel->ciview();
 		$this->load->view("template/head");
-        $this->load->view("template/view");
+        $this->load->view("template/view",$data);
 		$this->load->view('template/footer');
 	}
     public function dupdate()

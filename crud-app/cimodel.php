bf4fdfdde8f8dd0156bc4553f1dfda52ee941630
Punit Katiyar <punit.kat@gmail.com>
2022-01-20 17:59:50 +0530

    Update cimodel.php

--- crud-app/cimodel.php
+++ crud-app/cimodel.php
@@ -7,19 +7,35 @@ class Cimodel extends CI_Model {
         $this->db->insert('user',$data);
 
 	}
-    public function ciview()
+    public function ciview($data=null)
 	{
+		if($data==null)
+		{
         $query=$this->db->get('user');
         $res=$query->result_array($query);
         return $res;
+		}
+		else{
+			$this->db->where('uid',$data);
+			$query=$this->db->get('user');
+        	$res=$query->result_array($query);
+        	return $res;
+		}
+		
 
 	}
     public function ciupdate()
 	{
+		$this->db->where('uid',$did);
+		$this->db->delete('user',$data);
 
 	}
-    public function cidelete()
+    public function cidelete($did)
 	{
+		$this->db->where('uid',$did);
+		$this->db->delete('user');
+                            
+		
 
 	}
 }

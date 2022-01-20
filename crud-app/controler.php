76b81a5f6f92cd6197b2d1f5ff60ba111ab96262
Punit Katiyar <punit.kat@gmail.com>
2022-01-20 17:59:28 +0530

    Update controler.php

--- crud-app/controler.php
+++ crud-app/controler.php
@@ -8,8 +8,6 @@ class Crud extends CI_Controller {
         $this->load->model('Cimodel');
 
     }
-
-
 	public function index()
 	{
         $this->load->view("template/head");
@@ -42,14 +40,43 @@ class Crud extends CI_Controller {
 	}
     public function dupdate()
 	{
+		if(isset($_POST['godata']))
+		{
+			$data['uname']=$this->input->post('uname');
+			$data['uemail']=$this->input->post('uemail');
+			$data['upass']=$this->input->post('upass');
+			$data['ucity']=$this->input->post('ucity');
+			$data['ucourse']=$this->input->post('ucourse');
+			$this->Cimodel->ciupdate($data);
+		}
 		$this->load->view("template/head");
-        $this->load->view("template/update");
+		if(@$_GET['action']=='update')
+		{
+			$uuid=$_GET['uuid'];
+
+			$data['arr']=$this->Cimodel->ciview($uuid);
+			$this->load->view('template/edit',$data);
+		}
+		else
+		{
+			$data['arr']=$this->Cimodel->ciview();
+        	$this->load->view("template/update",$data);
+		}
 		$this->load->view('template/footer');
+		
 	}
     public function ddelete()
 	{
+		if(@$_GET['action']=='delete')
+		{
+			$did=$_GET['did'];
+			$this->Cimodel->cidelete($did);
+
+			
+		}
+		$data['arr']=$this->Cimodel->ciview();
 		$this->load->view("template/head");
-        $this->load->view("template/delete");
+        $this->load->view("template/delete",$data);
 		$this->load->view('template/footer');
 	}
 }
